02aea74fde0f9644bc2b0b8d05567b8c85c6a31b
Daboy <catalunesroy@gmail.com>
2023-10-04 09:51:29 +0800

    pagination-added

--- users/views.py
+++ users/views.py
@@ -9,6 +9,8 @@ from django.contrib.auth import authenticate, login, logout
 from django.shortcuts import render, get_object_or_404
 from .models import User
 from rest_framework import status
+from django.core.paginator import Paginator
+
 
 #import from form
 from .forms import create_user_form
